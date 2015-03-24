af07289ef114429ffcbd14ec55156f2bd9516ecf
JeroenBrinkman <s1366491@student.utwente.nl>
2015-03-24 12:49:21 +0100

    beetje component

--- ontwerpproject/src/model/Component.java
+++ ontwerpproject/src/model/Component.java
@@ -1,5 +1,20 @@
 package model;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public abstract class Component {
-	
+	private InetAddress adr;
+	private int id;
+
+	public Component(String ip, int id) {
+		this.id = id;
+
+		try {
+			adr = InetAddress.getByName(ip);
+		} catch (UnknownHostException e) {
+			System.out.println("invalid ip on component : " + id);
+		}
+	}
+
 }
