2ae01d5cc6667bc116da068a8244dc4833092815
JeroenBrinkman <s1366491@student.utwente.nl>
2015-03-24 13:05:32 +0100

    model dingen

--- ontwerpproject/src/model/Component.java
+++ ontwerpproject/src/model/Component.java
@@ -2,19 +2,27 @@ package model;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.lang.String;
+import java.util.Date;
 
 public abstract class Component {
 	private InetAddress adr;
 	private int id;
+	protected String[] keyList;
 
 	public Component(String ip, int id) {
 		this.id = id;
-
 		try {
 			adr = InetAddress.getByName(ip);
 		} catch (UnknownHostException e) {
 			System.out.println("invalid ip on component : " + id);
 		}
 	}
-
+	protected abstract void update(String type, int value, Date da);//hoeft wss geen abstraact te blijven
+	
+	public abstract void update(String message);//hoeft wss geen abstraact te blijven
+	
+	public abstract void parseInput();//waarschijnlijkt een array return
+	
+	public abstract void compressDatabase();//hoeft wss geen abstraact te blijven
 }
--- ontwerpproject/src/model/Manager.java
+++ ontwerpproject/src/model/Manager.java
@@ -1,10 +1,36 @@
 package model;
 
+import java.util.Date;
+
 public class Manager extends Component {
 
 	public Manager(String ip, int id) {
 		super(ip, id);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void parseInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void update(String type, int value, Date da) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void compressDatabase() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
