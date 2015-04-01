727236d21c0c01c0f18850bdfcfd853f579bca2d
Thijs <jemoederopeenbromfiets@live.nl>
2015-04-01 12:25:07 +0200

    JavaDoc Globals

--- ontwerpproject/src/global/Globals.java
+++ ontwerpproject/src/global/Globals.java
@@ -5,13 +5,19 @@ public class Globals {
 	public static final String[] workerKeys = { "key1", "key2" };
 	
 	/**
-	 * Array indices test
+	 * Array indices to send from retriever to the update method.
+	 * The retriever receives data from a component in text format.
+	 * The retriever parses the text input into an array.
+	 * The indices of each attribute of the component are set.
+	 * I.e. each attribute has a fixed position in the array.
+	 * Note that for different component types, different lengths of arrays are needed.
+	 * This can be traced back through the UPDATE_COMP_TYPE.
 	 */
-	public static final int UPDATE_COMP = 0;
-	public static final int UPDATE_TIME = 1;
-	public static final int UPDATE_CPU = 2;
-	public static final int UPDATE_MEM = 3;
-	public static final int UPDATE_DISK = 4;
+	public static final int UPDATE_INDEX_COMP_TYPE = 0;
+	public static final int UPDATE_INDEX_TIME = 1;
+	public static final int UPDATE_INDEX_CPU = 2;
+	public static final int UPDATE_INDEX_MEM = 3;
+	public static final int UPDATE_INDEX_DISK = 4;
 
 	public static String[] getKeys(String cl) {
 		if (cl.equals("worker")) {
