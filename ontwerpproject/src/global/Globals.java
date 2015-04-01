e15743810172fa78e1aa088534335a103434507e
Thijs <jemoederopeenbromfiets@live.nl>
2015-04-01 12:03:48 +0200

    Global variables
    asdf

--- ontwerpproject/src/global/Globals.java
+++ ontwerpproject/src/global/Globals.java
@@ -3,6 +3,15 @@ package global;
 public class Globals {
 	public static final String[] componentTypes = { "swag", "yolo" };
 	public static final String[] workerKeys = { "key1", "key2" };
+	
+	/**
+	 * Array indices
+	 */
+	public static final int UPDATE_COMP = 0;
+	public static final int UPDATE_TIME = 1;
+	public static final int UPDATE_CPU = 2;
+	public static final int UPDATE_MEM = 3;
+	public static final int UPDATE_DISK = 4;
 
 	public static String[] getKeys(String cl) {
 		if (cl.equals("worker")) {
