3ade4bb5e594e8176e90110b20ee0769630e12d0
anirudh55 <anirudh.ksg@gmail.com>
2015-04-01 12:24:38 +0200

    Retriever

--- ontwerpproject/src/controller/Retriever.java
+++ ontwerpproject/src/controller/Retriever.java
@@ -1,5 +1,50 @@
 package controller;
 
-public class Retriever {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+public class Retriever {
+	private InetAddress adr;
+	private int id;
+	private long[] data;
+	
+	/**
+	 * @require 
+	 * @ensure
+	 * @param ip
+	 * @param id
+	 */
+	public Retriever(String ip, int id){
+		this.id = id;
+		
+		try {
+			adr = InetAddress.getByName(ip);
+		} catch (UnknownHostException e) {
+			System.out.println("invalid ip on component : " + id);
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void updateData(int key, long value){
+		data[key] = value;
+	}
+	
+	/**
+	 * 
+	 */
+	public void getData(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void pushData(){
+		
+	}
 }
