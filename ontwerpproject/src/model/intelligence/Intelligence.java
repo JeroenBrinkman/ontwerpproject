c0303a74fb31d3f827e28aa3fec469a1d74525c2
JeroenBrinkman <s1366491@student.utwente.nl>
2015-05-07 15:57:38 +0200

    we kunnen nu mails sturen

--- ontwerpproject/src/model/intelligence/Intelligence.java
+++ ontwerpproject/src/model/intelligence/Intelligence.java
@@ -13,33 +13,37 @@ public abstract class Intelligence {
 	}
 
 	public void errorMail(String message, String subject) {
-		String to = "jeroen.brinkman@home.nl";
-		String from = "hoi@hai.nl";
-		String host = "localhost";
-		Properties properties = System.getProperties();
-		properties.setProperty("mail.smtp.host", host);
-		Session session = Session.getInstance(properties);
-		try {
-			MimeMessage email = new MimeMessage(session);
-			email.setFrom(new InternetAddress(from));
-			email.addRecipient(Message.RecipientType.TO,
-					new InternetAddress(to));
-			if (subject == null) {
-				email.setSubject("Alles kapot");
-			} else {
-				email.setSubject(subject);
-			}
-			if (message == null) {
-				email.setText("niks meer heel");
-			} else {
-				email.setText(message);
-			}
+		// TODO make this global
+		String to = "test@test.test";//needs to be valid though
+		String from = "monitoringontwerpproject@gmail.com";
+		final String username = "monitoringontwerpproject@gmail.com";
+		final String password = "T3st1234";
+		String host = "smtp.gmail.com";
 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "587");
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				});
+
+		try {
+			Message email = new MimeMessage(session);
+			email.setFrom(new InternetAddress(from));
+			email.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(to));
+			email.setSubject("Testing Subject");
+			email.setText("Hello, this is sample for to check send "
+					+ "email using JavaMailAPI ");
 			Transport.send(email);
-		} catch (MessagingException mex) {
-			mex.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
 		}
-		//TODO try this http://www.tutorialspoint.com/javamail_api/javamail_api_gmail_smtp_server.htm
 	}
 
 	public void errorPopup() {
