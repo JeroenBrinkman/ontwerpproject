d596adb70393818d22e53e40e0b6c91f0f66b5b3
JeroenBrinkman <s1366491@student.utwente.nl>
2015-06-10 13:39:16 +0200

    gui start, weet niet wat er allemaal in zou moeten, morgen ff vragen

--- ontwerpproject/src/view/GUI.java
+++ ontwerpproject/src/view/GUI.java
@@ -1,5 +1,33 @@
 package view;
 
+//Imports are listed in full to show what's being used //could just import javax.swing.* and java.awt.* etc.. 
+import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class GUI {
+	// Note: Typically the main method will be in a
+	// separate class. As this is a simple one class
+	// example it's all in the one class.
+	public static void main(String[] args) {
+		new GUI();
+	}
+
+	public GUI() {
+		JFrame guiFrame = new JFrame();
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Monitoring system");
+		guiFrame.setSize(800, 800);
+		guiFrame.setLocationRelativeTo(null);
+		GridLayout g = new GridLayout(2,2);
+		guiFrame.setLayout(g);
+
+
+
+
+		// make sure the JFrame is visible
+		guiFrame.setVisible(true);
+	}
 
 }
